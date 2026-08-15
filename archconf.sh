ad8ea33525f3632ccaa143505e1009f406511b92
Methusan105 <91547533+Methusan105@users.noreply.github.com>
2026-08-15 13:55:46 +0200

    Add cache control to curl commands in archconf.sh
    Updated curl commands to include cache control headers and timestamp.

--- archconf.sh
+++ archconf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-curl -fLO https://raw.githubusercontent.com/Methusan105/archconfig/main/user_configuration.json
-curl -fLO https://raw.githubusercontent.com/Methusan105/archconfig/main/user_credentials.json
+curl -H "Cache-Control: no-cache, no-store" -fLO "https://raw.githubusercontent.com/Methusan105/archconfig/main/user_configuration.json?$(date +%s)"
+curl -H "Cache-Control: no-cache, no-store" -fLO "https://raw.githubusercontent.com/Methusan105/archconfig/main/user_credentials.json?$(date +%s)"
 
 archinstall --config user_configuration.json --creds user_credentials.json
