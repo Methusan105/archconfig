ec81b6e3f4ad4a469f08da1d475df1136f551492
Methusan105 <91547533+Methusan105@users.noreply.github.com>
2026-09-03 12:35:34 +0200

    Update install pkgs after timeshift.sh

--- install pkgs after timeshift.sh
+++ install pkgs after timeshift.sh
@@ -1,1 +1,2 @@
-sudo pacman -S archconfig-cli gh-uploader-custom ffmpeg-gui-ver-methu warp-cloudflare-gui process-lasso-linux
+sudo pacman -Sy
+sudo pacman -S --needed archconfig-cli gh-uploader-custom ffmpeg-gui-ver-methu warp-cloudflare-gui process-lasso-linux
