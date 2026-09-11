b9833bfee043770982aa755b0bdeaedc5bcb88bb
Methusan105 <91547533+Methusan105@users.noreply.github.com>
2026-09-11 13:03:47 +0200

    Update install pkgs after timeshift.sh

--- install pkgs after timeshift.sh
+++ install pkgs after timeshift.sh
@@ -1,2 +1,2 @@
 sudo pacman -Sy
-sudo pacman -S --needed archconfig-cli github-release-downloader github-release-uploader ffmpeg-gui-ver-methu warp-cloudflare-gui process-lasso-linux
+sudo pacman -S --needed archconfig-cli github-release-downloader github-release-uploader ffmpeg-gui-ver-methu warp-cloudflare-gui process-lasso-linux filelight stream-game-installer
